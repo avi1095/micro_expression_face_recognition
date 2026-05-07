55cf1fae17cd9d7195cd19028c9e68999b6cf781
Aditya Ingle <156649327+AVI1095@users.noreply.github.com>
2026-05-07 15:55:28 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,20 +1,3 @@
-# import libreface
-# import os
-#
-# video_path = r"C:/Users/ASUS/PycharmProjects/Micro Expression Face Recognition/gallery/video_me/perpvid.mp4"
-# output_csv_path = r"C:/Users/ASUS/PycharmProjects/Micro Expression Face Recognition/outputs/perp_video_result.csv"
-#
-# print("Video file exists?", os.path.exists(video_path))
-#
-# libreface.get_facial_attributes(
-#     video_path,
-#     output_save_path=output_csv_path,
-#     device="cpu",
-#     batch_size=64,
-#     num_workers=2
-# )
-#
-# print("CSV file exists?", os.path.exists(output_csv_path))
 import libreface
 import os
 
@@ -24,7 +7,7 @@ output_csv_path = "//outputs/my_video_result_me.csv"
 libreface.get_facial_attributes(
     video_path,
     output_save_path=output_csv_path,
-    device="cuda:0",         # use CPU if you prefer, but your RTX 4060 is excellent
+    device="cuda:0",      
     batch_size=96,
     num_workers=6
 )
